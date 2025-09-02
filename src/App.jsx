4f1e948f2cb86820ea362a6d348533fbd51bb45b
jose pardo <s2500274@edu.bc.fi>
2025-09-02 09:47:25 +0300

    unicafe step 5 data display changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,47 @@ import { useState } from "react";
 
 const Statistics = (props) => {
   const total = props.good + props.neutral + props.bad;
-  const average = Math.round(((props.good - props.bad) / total) * 100) / 100;
-  const positive = Math.round(
-    ((total - props.neutral - props.bad) / total) * 100
-  );
+  const average = (props.good - props.bad) / total;
+  const positive = (total - props.neutral - props.bad) / total;
+
+  // Convert average to two decimal points, with trailing zeroes
+  // Set displayed average to 0 if average is equal to a falsy value (such as NaN)
+  const displayAverage =
+    (average
+      ? average.toLocaleString("en", {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        })
+      : 0) + " pts";
+  // Convert positive to percentage, same fallback value of 0 as above
+  const displayPositive =
+    (positive
+      ? (positive * 100).toLocaleString("en", {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        })
+      : 0) + "%";
+
   return (
     <div>
       <h2>Statistics</h2>
       {total ? (
         <>
-          <StatisticsLine text="good" value={props.good} />
-          <StatisticsLine text="neutral" value={props.neutral} />
-          <StatisticsLine text="bad" value={props.bad} />
-          <StatisticsLine text="total" value={total} />
-          <StatisticsLine text="average" value={average ? average : 0} />
-          <StatisticsLine
-            text="positive"
-            value={(positive ? positive : 0) + "%"}
-          />
+          <StatisticLine text="Good:" value={props.good} />
+          <StatisticLine text="Neutral:" value={props.neutral} />
+          <StatisticLine text="Bad:" value={props.bad} />
+          <StatisticLine text="Total:" value={total} />
+          <StatisticLine text="Average:" value={displayAverage} />
+          <StatisticLine text="Positive:" value={displayPositive} />
         </>
       ) : (
-        <p>No Feedback Given</p>
+        <p>No feedback given</p>
       )}
     </div>
   );
 };
 
-const StatisticsLine = (props) => {
+const StatisticLine = (props) => {
   return (
     <div>
       <span>{props.text} </span>
